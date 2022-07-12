9f6b6bb8c88f88ec9347966d594732cf23dff5d5
awadheshkumarmnnit <69567671+awadheshkumarmnnit@users.noreply.github.com>
2022-07-13 00:01:10 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,13 @@ import {getAuth,GoogleAuthProvider,signInWithPopup,signInWithEmailAndPassword,cr
 import {getDatabase} from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   
+    apiKey: "AIzaSyAbSTsn_YKsl0J7e-pg5GUuQ72PXJEb1WA",
+  authDomain: "goparty-484f1.firebaseapp.com",
+  projectId: "goparty-484f1",
+  storageBucket: "goparty-484f1.appspot.com",
+  messagingSenderId: "193370412369",
+  appId: "1:193370412369:web:25ab30443fb97885d22f70",
+  measurementId: "G-QM1RWPBE9M" 
   };
 
 initializeApp(firebaseConfig);
